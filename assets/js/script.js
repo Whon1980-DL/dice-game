a0c3c8aac582ee6880c36e011a1dad8dc437a331
Supitpong Wongsaroj <ewrctysf@students.codeinstitute.net>
2024-10-12 06:19:30 +0000

    Add dicerolling function and add animation to dice rolling process

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-//player Name 
-let player1 = "Player 1";
-let player2 = "Player 2";
-
+//Player Name Editing
 function editNames() {
+    let player1 = "Player 1";
+    let player2 = "Player 2";
+
     player1 = prompt("Change player 1 Name");
     player2 = prompt("Change player 2 Name");
 
@@ -15,4 +15,21 @@ function editNames() {
 
     document.querySelector("p.player2")
         .innerHTML = player2;
+}
+
+//Dice rolling 
+function roolTheDice() {
+    let diceNum1 = document.querySelector(".img1");
+    let diceNum2 = document.querySelector(".img2");
+
+    diceNum1.setAttribute("src","diceroll.gif");
+    diceNum2.setAttribute("src","diceroll.gif");
+
+    let result = document.querySelector('h1');
+    setTimeout(() => {
+        let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+        let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+    }, 2500);
+    
 }
