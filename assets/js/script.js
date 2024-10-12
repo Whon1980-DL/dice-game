99eb534e533d9f5a08592fa75e1a04876d00fb10
Supitpong Wongsaroj <ewrctysf@students.codeinstitute.net>
2024-10-12 07:07:12 +0000

    Fix code for declaring winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
-//Player Name Editing
-function editNames() {
-    let player1 = "Player 1";
-    let player2 = "Player 2";
+//Player name has to be global variabless as they 
+//need to be reassigned in the rollTheDice function
+let player1 = "Player 1";
+let player2 = "Player 2";
 
+//player Nmae editing
+function editNames() {
     player1 = prompt("Change player 1 Name");
     player2 = prompt("Change player 2 Name");
 
@@ -18,32 +20,33 @@ function editNames() {
 }
 
 //Dice rolling 
-function roolTheDice() {
+function rollTheDice() {
     let diceNum1 = document.querySelector(".img1");
     let diceNum2 = document.querySelector(".img2");
 
-    diceNum1.setAttribute("src","diceroll.gif");
-    diceNum2.setAttribute("src","diceroll.gif");
+    diceNum1.setAttribute("src","assets/image/diceroll.gif");
+    diceNum2.setAttribute("src","assets/image/diceroll.gif");
 
     let result = document.querySelector('h1');
+    result.innerHTML = ""
     setTimeout(() => {
         let randomNumber1 = Math.floor(Math.random() * 6) + 1;
         let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-        diceNum1.setAttribute('src','dice'+randomNumber1+'.png');
-        diceNum2.setAttribute('src','dice'+randomNumber2+'.png');
+        diceNum1.setAttribute('src','assets/image/dice' + randomNumber1 + '.png');
+        diceNum2.setAttribute('src','assets/image/dice' + randomNumber2 + '.png');
 
         //Determine the winner
-        if(randomNumber1 === randomNumber2) {
+        if (randomNumber1 === randomNumber2) {
             result.innerHTML = "Draw!"
         } 
-        else if(randomNumber1 > randomNumber2) {
-            result.innerHTML = (player1+" WIN!");
+        else if (randomNumber1 > randomNumber2) {
+            result.innerHTML = (player1 + " WIN!");
         }
         else {
-            result.innerHTML = (player2+" WIN!");
+            result.innerHTML = (player2 + " WIN!");
         }
 
-    }, 2500);
+    }, 1000);
     
 }
